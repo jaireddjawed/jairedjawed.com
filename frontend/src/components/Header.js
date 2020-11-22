9d407e423d3a878c772f341b6c76411abfe6ce7c
Jaired Jawed <jairedjawed@icloud.com>
2020-11-21 18:02:33 -0800

    Added Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ParticlesBg  from "particles-bg";
 
-function Header() {
+function Header({ data }) {
    return (
       <header id="home">
          <ParticlesBg type="circle" bg={true} />
@@ -10,23 +10,29 @@ function Header() {
             <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
 
             <ul id="nav" className="nav">
-               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+               <li><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
-               <li><a className="smoothscroll" href="#resume">Resume</a></li>
                <li><a className="smoothscroll" href="#portfolio">Portfolio</a></li>
+               <li><a className="smoothscroll" href="#resume">Resume</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
          </nav>
 
          <div className="row banner">
-         
             <div className="banner-text">
                <h1 className="responsive-headline">Jaired Jawed</h1>
                <h3>A responsible, detailed-oriented and steadfast problem solver with the ability to quickly adapt to new emerging frameworks. Proficient in a range of web technologies including React, Node.js, and Python.</h3>
                <hr />
                <ul className="social">
-                  <a href="" className="button btn project-btn"><i className="fa fa-download"></i>Resume</a>
-                  <a href="https://github.com/jaireddjawed" className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
+                  <li>
+                     <a href={data.resumedownload} className="button btn project-btn"><i className="fa fa-download" /> Resume</a>
+                  </li>
+                  <li>
+                     <a href="https://github.com/jaireddjawed" className="button btn github-btn"><i className="fa fa-github" /> Github</a>
+                  </li>
+                  <li>
+                  <a href="https://www.linkedin.com/in/jaired/" className="button btn linkedin-btn"><i className="fa fa-linkedin" /> LinkedIn</a>
+                  </li>
                </ul>
             </div>
          </div>
