446bc400773f24ef2cca4c8e9f25f133a5a0f560
Jaired Jawed <jairedjawed@icloud.com>
2020-11-21 18:02:40 -0800

    Added Resume

--- frontend/src/components/Resume.js
+++ frontend/src/components/Resume.js
@@ -1,68 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Resume extends Component {
-
-  getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-  render() {
-
-    if (this.props.data) {
-      var skillmessage = this.props.data.skillmessage;
-
-      var education = this.props.data.education.map(function (education) {
-        return (
-          <div key={education.school}>
-            <h3>{education.school}</h3>
-            <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-            <p>{education.description}</p>
-          </div>
-        );
-      });
-
-      var work = this.props.data.work.map(function (work) {
-        return <div key={work.company}><h3>{work.company}</h3>
-          <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-          <p>{work.description}</p>
-        </div>
-      })
-    }
-
-    return (
-      <section id="resume">
-        <div className="row work">
-
-          <div className="three columns header-col">
-            <h1><span>Experience</span></h1>
-          </div>
-
-          <div className="nine columns main-col">
-            {work}
-          </div>
+function Resume({ data: { education, work } }) {
+  return (
+    <section id="resume">
+       <div className="row education">
+        <div className="three columns header-col">
+          <h1><span>Education</span></h1>
         </div>
 
-        <div className="row education">
-          <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-          </div>
-
-          <div className="nine columns main-col">
-            <div className="row item">
-              <div className="twelve columns">
-                {education}
-              </div>
+        <div className="nine columns main-col">
+          <div className="row item">
+            <div className="twelve columns">
+               {education.map(({ school, degree, graduated }, i) => (
+                  <div key={String(i)}>
+                    <h3>{school}</h3>
+                    <p className="info">{degree} <span>&bull;</span><em className="date">{graduated}</em></p>
+                  </div>
+                ))}
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+      <div className="row work">
+
+        <div className="three columns header-col">
+          <h1><span>Experience</span></h1>
+        </div>
+
+        <div className="nine columns main-col">
+          {work.map(({ company, title, years, details }, i) => (
+            <div key={String(i)}>
+              <h3>{company}</h3>
+              <p className="info">{title}<span>&bull;</span><em className="date">{years}</em></p>
+              <ul className="work-details">
+                {details.map(detail => <li key={detail}>{detail}</li>)}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Resume;
