2aedfd735a651cfa377669271a83bc4f8de35d3d
Jaired Jawed <me@jairedjawed.com>
2024-12-12 00:17:03 +0200

    Moved old folder into src

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,1 @@
+(()=>{var r={};r.id=636,r.ids=[636],r.modules={7954:(r,e,s)=>{"use strict";s.r(e),s.d(e,{default:()=>u});var t=s(8732);function u({Component:r,pageProps:e}){return(0,t.jsx)(r,{...e})}s(9361)},9361:()=>{},8732:r=>{"use strict";r.exports=require("react/jsx-runtime")}};var e=require("../webpack-runtime.js");e.C(r);var s=e(e.s=7954);module.exports=s})();
