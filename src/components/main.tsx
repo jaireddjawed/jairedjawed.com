1e3488107b81bfbaab7631169ea391bd09d73050
Jaired Jawed <me@jairedjawed.com>
2024-12-15 08:29:43 +0100

    Update page to include light mode

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -4,7 +4,7 @@ const ParticlesBg = dynamic(() => import("particles-bg"), { ssr: false });
 
 function Banner() {
   return (
-    <div className="border border-white p-3 m-2 mb-6 rounded-full">
+    <div className="border border-white p-3 m-2 mb-6 rounded-full text-center">
       Tutoring service resumes on <strong>Jan 6, 2025</strong>. Reserve a
       tutoring session in advance{" "}
       <a href="https://tutoring.jairedjawed.com" className="text-blue-600">
@@ -20,9 +20,11 @@ export default function Main() {
     <div className="w-screen h-screen flex flex-col items-center justify-center gap-3">
       <ParticlesBg bg={true} type="polygon" />
       <Banner />
-      <h1 className="text-slate-100 font-semibold text-7xl">Jaired Jawed</h1>
-      <h3 className="text-slate-300 font-semibold text-2xl">
-        Software Engineer at HashiCorp, an IBM company
+      <h1 className="dark:text-slate-100 font-semibold text-7xl text-center">
+        Jaired Jawed
+      </h1>
+      <h3 className="dark:text-slate-300 font-semibold text-2xl text-center">
+        Software Engineer at HashiCorp, Inc.
       </h3>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Main from "@/components/main";
 
 export default function Index() {
   return (
     <>
+      <Head>
+        <title>Jaired Jawed | Software Engineer</title>
+      </Head>
       <Main />
     </>
   );
