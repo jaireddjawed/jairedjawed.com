5714f0bd09a160dc0609d5614d66f6ede5a78210
Jaired Jawed <me@jairedjawed.com>
2026-04-05 21:43:26 -0500

    remove tutoring service banner

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,25 +1,12 @@
 import dynamic from "next/dynamic";
+import {JSX} from "react";
 
 const ParticlesBg = dynamic(() => import("particles-bg"), { ssr: false });
 
-function Banner() {
-  return (
-    <div className="border border-white p-3 m-2 mb-6 rounded-full text-center">
-      Tutoring service resumes on <strong>Jan 6, 2025</strong>. Reserve a
-      tutoring session in advance{" "}
-      <a href="https://tutoring.jairedjawed.com" className="text-blue-600">
-        here
-      </a>
-      .
-    </div>
-  );
-}
-
-export default function Main() {
+export default function Main(): JSX.Element  {
   return (
     <div className="w-screen h-screen flex flex-col items-center justify-center gap-3">
       <ParticlesBg bg={true} type="polygon" />
-      <Banner />
       <h1 className="dark:text-slate-100 font-semibold text-7xl text-center">
         Jaired Jawed
       </h1>
